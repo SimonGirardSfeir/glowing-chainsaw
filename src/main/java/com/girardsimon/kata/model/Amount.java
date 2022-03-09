d1b29dce0ebfe1163e429f80026f71f0ca7eb3e5
Simon Girard <simon.girard1991@gmail.com>
2022-03-09 17:02:49 +0100

    amount_should_be_equal_to_other_amount_when_value_is_the_same SUCCESS

--- src/main/java/com/girardsimon/kata/model/Amount.java
+++ src/main/java/com/girardsimon/kata/model/Amount.java
@@ -4,4 +4,14 @@ public class Amount {
 
     public Amount(int value) {
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Amount;
+    }
 }
