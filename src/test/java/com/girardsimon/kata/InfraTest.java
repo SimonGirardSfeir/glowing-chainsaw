b3debae4d344d0a95167fb7007d1ae5f8ab7a50b
Simon Girard <simon.girard1991@gmail.com>
2022-03-09 15:49:35 +0100

    first test that fails

---
+++ src/test/java/com/girardsimon/kata/InfraTest.java
@@ -0,0 +1,13 @@
+package com.girardsimon.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class InfraTest {
+
+    @Test
+    void shouldFail() {
+        fail();
+    }
+}
